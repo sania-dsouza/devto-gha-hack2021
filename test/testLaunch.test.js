e7f4a93f53c1cfa7a446858587f447de8a25af32
Sania <sania.d@myplanet.com>
2021-12-06 18:29:23 -0500

    Added unit tests

---
+++ test/testLaunch.test.js
@@ -0,0 +1,26 @@
+const { TestSuite, InputType } = require('@jovotech/framework');
+
+const testSuite = new TestSuite();
+
+test('should launch when prompted', async () => {
+
+  const { output } = await testSuite.run({
+    type: InputType.Launch, // or 'LAUNCH'
+  });
+
+  expect(output).toEqual([
+    {
+      message: 'Welcome to NutriPlanner app! Do you want to save your cal intake today?',
+      quickReplies: ['yes', 'no'],
+      listen: true,
+    },
+  ]);
+
+  //console.log(output);
+
+  expect(output[0]["message"]).toBeDefined();
+
+});
+
+
+
