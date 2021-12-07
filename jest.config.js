e5d638cda82b3b5b9ab7741bb937dca55ee7893e
Sania <sania.d@myplanet.com>
2021-12-06 21:39:35 -0500

    Changed path for jest test report

--- jest.config.js
+++ jest.config.js
@@ -102,7 +102,8 @@ module.exports = {
   "reporters": [
     "default",
     ["./node_modules/jest-html-reporter", {
-      "pageTitle": "Test Report"
+      "pageTitle": "Test Report",
+      "outputPath" : "./reports/unit-test-report.html"
     }]
   ],
 
