2226361c91425d3cdee76371bde943ab4f49241c
Sania <sania.d@myplanet.com>
2021-12-08 16:24:09 -0500

    Added virtual-alexa

---
+++ virtual-tests/va-test1.test.js
@@ -0,0 +1,33 @@
+const expect = require("chai").expect;
+const VirtualAlexa = require("virtual-alexa").VirtualAlexa;
+
+describe("Test the nutriplanner app skills", function() {
+    let alexa;
+    
+    beforeEach(function () {
+        alexa = VirtualAlexa.Builder()
+            .handler("src/index.handler") // Lambda function file and name
+            .interactionModelFile("./build/platform.alexa/skill-package/interactionModels/custom/en-US.json")
+            .create();
+    });
+
+    it("Should agree that true is true", function() {
+        // console.log(alexa);
+        expect(true).to.be.true;
+    });
+
+    it("Should launch the skill and get a response", function(done) {
+        alexa.filter(function(request) {
+            request.context.System.device.supportedInterfaces.Display = {};
+            request.context.System.device.supportedInterfaces.VideoApp = {};
+            return request;
+        }).launch().then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("nutri planner");
+            done();
+        });
+    });
+
+
+    
+});
